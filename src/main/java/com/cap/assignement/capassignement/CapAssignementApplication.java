8b560f06e2c7d8c3ba6efbfb8567b3b55a87cdb0
nidy <idykaya@gmail.com>
2024-12-29 20:08:53 +0100

    initial commit

---
+++ src/main/java/com/cap/assignement/capassignement/CapAssignementApplication.java
@@ -0,0 +1,13 @@
+package com.cap.assignement.capassignement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapAssignementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapAssignementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cap/assignement/capassignement/CapAssignementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.assignement.capassignement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapAssignementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

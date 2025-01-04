b0a1792449249c8b9b5008c94268aa8e3395c330
nidy <idykaya@gmail.com>
2025-01-05 00:12:50 +0100

    modify swagger

--- src/main/java/com/cap/assignement/capassignement/controllers/CustomerManagementController.java
+++ src/main/java/com/cap/assignement/capassignement/controllers/CustomerManagementController.java
@@ -3,6 +3,12 @@ package com.cap.assignement.capassignement.controllers;
 import com.cap.assignement.capassignement.pojo.Customer;
 import com.cap.assignement.capassignement.service.AccountService;
 import com.cap.assignement.capassignement.service.CustomerService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,17 +28,56 @@ public class CustomerManagementController {
     }
 
     @GetMapping("/health-check")
-    public Boolean index() {
-        return true;
+    public ResponseEntity<String> index() {
+         return new ResponseEntity<>("", HttpStatus.OK);
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Customer not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "bad request",
+                    content = @Content)
+
+    })
     @PostMapping("/accounts")
-    public void account(Integer idCustomer,Integer credit ) {
-        accountService.createAccount(idCustomer, credit);
+    public ResponseEntity<String> account(Integer idCustomer, Integer credit ) {
+        if (idCustomer == null || idCustomer < 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        boolean status = accountService.createAccount(idCustomer, credit);
+        if (status) {
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
+        }
+
     }
 
+
+
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Customer.class)) }),
+            @ApiResponse(responseCode = "404", description = "Customer not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "bad request",
+                    content = @Content)
+
+    })
     @GetMapping("/customers")
-    public Customer customer(Integer idCustomer) {
-        return customerService.showInfo(idCustomer);
+    public ResponseEntity<Customer> customer(Integer idCustomer) {
+        if (idCustomer == null || idCustomer < 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Customer customer = customerService.showInfo(idCustomer);
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(customer,HttpStatus.OK);
     }
 }
